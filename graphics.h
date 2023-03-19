8626698f0c97165dcb888aae5ff610f670dafef6
Subarasheese <35178075+Subarasheese@users.noreply.github.com>
2023-03-19 17:55:39 -0300

    Create graphics.h

---
+++ graphics.h
@@ -0,0 +1,12 @@
+#ifndef GRAPHICS_H
+#define GRAPHICS_H
+
+#include <stdbool.h>
+
+// Initialize graphics
+bool graphics_init();
+
+// Clean up resources
+void graphics_cleanup();
+
+#endif // GRAPHICS_H
