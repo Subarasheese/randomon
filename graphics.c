64bf714cc2350bcbcc13a5589e88aa27a4ff6079
Subarasheese <35178075+Subarasheese@users.noreply.github.com>
2023-03-19 18:31:12 -0300

    Update graphics.c

--- graphics.c
+++ graphics.c
@@ -4,8 +4,15 @@
 
 // Global variables
 SDL_Texture *g_player_texture = NULL;
+SDL_Texture *g_tileset_texture = NULL;
 
 static bool load_assets() {
+    // Load tileset
+    g_tileset_texture = load_texture("tileset.png");
+    if (!g_tileset_texture) {
+        fprintf(stderr, "Failed to load tileset!\n");
+        return false;
+    }
     // Load player sprite sheet
     g_player_texture = load_texture("player_spritesheet.png");
     if (!g_player_texture) {
@@ -61,6 +68,7 @@ static bool load_assets() {
 void graphics_cleanup() {
     // Free the player's texture
     SDL_DestroyTexture(g_player_texture);
+    SDL_DestroyTexture(g_tileset_texture);
 
     // Free other textures...
 }
@@ -91,6 +99,33 @@ void render_player(Player *player, SDL_Rect *camera) {
 
     // Render the player's sprite
     SDL_RenderCopy(g_renderer, g_player_texture, &src, &dest);
+}
+
+void render_overworld(Tile world[WORLD_HEIGHT][WORLD_WIDTH], SDL_Rect *camera) {
+    int tile_size = 32;
+
+    for (int y = 0; y < WORLD_HEIGHT; y++) {
+        for (int x = 0; x < WORLD_WIDTH; x++) {
+            // Calculate source rectangle based on tile ID
+            SDL_Rect src = {
+                world[y][x].id * tile_size,
+                0,
+                tile_size,
+                tile_size
+            };
+
+            // Calculate destination rectangle with camera's position offset
+            SDL_Rect dest = {
+                x * tile_size - camera->x,
+                y * tile_size - camera->y,
+                tile_size,
+                tile_size
+            };
+
+            // Render the tile
+            SDL_RenderCopy(g_renderer, g_tileset_texture, &src, &dest);
+        }
+    }
 }
 
 void render_enemies(SDL_Rect *camera) {
